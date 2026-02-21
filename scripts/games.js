641feab570eaefa914087ca010a2a1e410f71bf4
Salis_Silva <salissilvadev@gmail.com>
2026-02-21 12:40:40 -0300

    feito

--- scripts/games.js
+++ scripts/games.js
@@ -3,11 +3,17 @@ let games = [
   "/pages/games/roadrash/roadrash.html",
   "/pages/games/ocarina of time/ocarinaoftime.html",
   "/pages/games/tonyhawk2/tonyhawk2.html",
+  "/pages/games/street fighter 2/streetfighter2.html",
+  "/pages/games/super mario world/mario.html",
+  "/pages/games/sonic/sonic.html"
 ];
 
 
 function randomGame() {
     let randomIndex = Math.floor(Math.random() * games.length);
+    if (this.window.location.pathname === games[randomIndex]) {
+      randomIndex = (randomIndex + 1) % games.length; 
+    }
     window.location.href = games[randomIndex];
 }
 
